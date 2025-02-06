55e2dc057249a87dc557fa4edd6b028400e54407
kirtayinteractive <mhmd4lk_@outlook.com>
2025-02-06 18:34:01 +0300

    java core init

---
+++ readme.md
@@ -0,0 +1,9 @@
+# IBB Ecodation Java Core
+[GitHub](https://github.com/kirtayinteractive/ibb_java.git)
+
+#Git
+```sh
+git init
+```
+---
+
---
+++ src/main/java/com/muhammedalikirtay/_1_part_javatypes/_01_JavaTypes.java
@@ -0,0 +1,9 @@
+package com.muhammedalikirtay._1_part_javatypes;
+
+
+public class _01_JavaTypes {
+
+    public static void main(String[] args) {
+
+    }
+}
