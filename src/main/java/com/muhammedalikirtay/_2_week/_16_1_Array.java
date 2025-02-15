5d7ebdea73819e4b7956b8201f8d7fe90b8bf927
kirtayinteractive <mhmd4lk_@outlook.com>
2025-02-15 18:57:23 +0300

    java core init

--- src/main/java/com/muhammedalikirtay/_2_week/_16_1_Array.java
+++ src/main/java/com/muhammedalikirtay/_2_week/_16_1_Array.java
@@ -1,6 +1,9 @@
 package com.muhammedalikirtay._2_week;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 public class _16_1_Array {
     public static String[] arrayMethod1() throws ArrayIndexOutOfBoundsException{
@@ -43,6 +46,7 @@ public class _16_1_Array {
             System.out.println(temp);
         }
     }
+    //Array.sort küçükten büyüğe
     public  static void arrayMethod5(){
         String[] city=arrayMethod2();
         // Dizilerde Sıralama
@@ -51,9 +55,44 @@ public class _16_1_Array {
             System.out.println(temp);
         }
     }
+    //Array.sort büyükten küçüğe
+    public  static void arrayMethod6(){
+        String[] city=arrayMethod2();
+        // Dizilerde Sıralama
+        Arrays.sort(city, Collections.reverseOrder());
+        for(String temp:city){
+            System.out.println(temp);
+        }
+    }
+    //Random Numvber
+    public static int randomNumber(){
+
+        Random random=new Random();
+        int rndInt=random.nextInt(9)+1;
+        return rndInt;
+    }
+
+    //fill:
+    public  static void arrayMethod7(){
+        int[] number= new int[7];
+        //metodu tek bir değeri dizinin tüm elemanlarına atamak için tasarlanmıştır
+        Arrays.fill(number,randomNumber());
+
+        // iterative
+        for(int i=0;i<number.length;i++){
+            //number[i]=randomNumber(); //(1.Yol)
+            Arrays.setAll(number, data->randomNumber());//(2.Yol)
+        }
+        //foreach
+        for(int temp:number){
+            System.out.println(temp);
+        }
+    }
     public static void main(String[] args) {
         //arrayMethod3();
-        //arrayMethod4();
-        arrayMethod5();
+        //arrayMethod5();
+        //arrayMethod6();
+        arrayMethod7();
+
     }
 }
