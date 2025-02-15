f73998f8386c49b532f15f2ef1852e710d013913
kirtayinteractive <mhmd4lk_@outlook.com>
2025-02-15 17:19:28 +0300

    java core init

---
+++ src/main/java/com/muhammedalikirtay/_2_week/_12_2_StringFormat.java
@@ -0,0 +1,16 @@
+package com.muhammedalikirtay._2_week;
+
+import java.util.Formatter;
+
+public class _12_2_StringFormat {
+
+    public  static void formatter1(){
+        Formatter formatter=new Formatter();
+        formatter.format(" Merhabalar Adınız:%s, Numaranız:%d, T.C.:%d, Fiyat:%f","Muhammed Ali",21,12345,25.5);
+        System.out.println(formatter);
+    }
+
+    public static void main(String[] args) {
+        formatter1();
+    }
+}
