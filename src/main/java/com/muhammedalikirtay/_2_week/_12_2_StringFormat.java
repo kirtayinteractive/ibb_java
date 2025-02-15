3271bc4e2492d377245adb84460916955251b0af
kirtayinteractive <mhmd4lk_@outlook.com>
2025-02-15 17:31:06 +0300

    java core init

--- src/main/java/com/muhammedalikirtay/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/muhammedalikirtay/_2_week/_12_2_StringFormat.java
@@ -8,9 +8,21 @@ public class _12_2_StringFormat {
         Formatter formatter=new Formatter();
         formatter.format(" Merhabalar Adınız:%s, Numaranız:%d, T.C.:%d, Fiyat:%f","Muhammed Ali",21,12345,25.5);
         System.out.println(formatter);
+        formatter.close();
+    }
+
+    public  static void formatter2(){
+       String formatterString=String.format(" Merhabalar Adınız:%s, Numaranız:%d, T.C.:%d, Fiyat:%f","Muhammed Ali",21,12345,25.5);
+        System.out.println(formatterString);
+    }
+
+    public  static void formatter3(){
+
     }
 
     public static void main(String[] args) {
-        formatter1();
+        //formatter1();
+        //formatter2();
+        formatter3();
     }
 }
