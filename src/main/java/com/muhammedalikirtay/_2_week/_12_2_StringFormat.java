50cc0331ae348ac78e3ad31b9ab82996ec64104e
kirtayinteractive <mhmd4lk_@outlook.com>
2025-02-15 17:55:14 +0300

    java core init

--- src/main/java/com/muhammedalikirtay/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/muhammedalikirtay/_2_week/_12_2_StringFormat.java
@@ -5,24 +5,39 @@ import java.util.Formatter;
 public class _12_2_StringFormat {
 
     public  static void formatter1(){
+        //Eğer new Formatter yazarsak close() ile manuel
         Formatter formatter=new Formatter();
         formatter.format(" Merhabalar Adınız:%s, Numaranız:%d, T.C.:%d, Fiyat:%f","Muhammed Ali",21,12345,25.5);
         System.out.println(formatter);
         formatter.close();
     }
 
+
     public  static void formatter2(){
+        // Eğer String.format() close gerek yok GC(Garbage Collector) otomatik çalışır
        String formatterString=String.format(" Merhabalar Adınız:%s, Numaranız:%d, T.C.:%d, Fiyat:%f","Muhammed Ali",21,12345,25.5);
         System.out.println(formatterString);
     }
 
     public  static void formatter3(){
+        String formatterString = String.format("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Muhammed Ali", 123456, 25.5);
+        System.out.println(formatterString);
+    }
+    public  static void formatter4(){
 
+        String name = "Muhammed Ali";
+        int tcNumber = 123456;
+        double price = 25.5;
+        System.out.printf("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", name, tcNumber, price);
+        System.out.println();
+        System.out.printf("Merhabalar Adınız: %10s, T.C: %d, Fiyat: %f ", name, tcNumber, price); //%10s: Sağdan 10 karakter genişliğinde
+        System.out.println();
+        System.out.printf("%-15s %-15d %.3f ", name, tcNumber, price);//%-15s: Sola doğru yasla
     }
 
     public static void main(String[] args) {
         //formatter1();
         //formatter2();
-        formatter3();
+        formatter4();
     }
 }
